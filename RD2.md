68ebae22cfe0e991ddda57c7a414caf0ba00b744
leiflabianca <leif.labianca@gmail.com>
2024-10-08 21:16:57 -0400

    edited/fixed code and uploaded Leif's reflection

--- RD2.md
+++ RD2.md
@@ -1,7 +1,9 @@
 # Reflection Document
 
-* Drive Full Name  : 
-* Partner Full Name: 
-* Student ID: 
+* Drive Full Name  : Leif LaBianca
+* Partner Full Name: Max Rice
+* Student ID: 1928607
+
+In this assignment, we were supposed to perfect the use of both while and if-else statements. In doing so, we implemented two while statements, both of which utilized "passthru" variables to determine whether or not they would loop. We also used a variety of if-else statements to work through  the different necessary paths. Our results in the end matched what we expected and wanted, even with the fairly limited and frameworked test cases we used to determine the outcome. Along the way, we came across typecasting issues, which misread the input and crashed the program. We fixed these issues quickly after realizing that it was due to an incorrectly written variable. There are no key takeaways here that have not been reached in previous labs or programming assignments. I think that both me and my partner learned what we were supposed to regardless. Working with my partner was easy, even if it was mostly asynchronous. We understood what we needed to do and finished it up without much intervention.
 
 
--- main.py
+++ main.py
@@ -2,35 +2,47 @@
 #assignment due 11/7
 
 #Goal of lab is to calculate monthly cost of users payment based on data used, the package type, and if they can use a coupon
+passthru1 = "no"
+passthru2 = "no"
 package_type = input('Enter package type green, blue, or purple: ')
 package_type = package_type.lower()
 #sets values for the package types
-if package_type == ('green'):
-    monthly_payment = 49.99
-    provided_data = 2
-    extra_data = 15
-elif package_type == ('blue'):
-    monthly_payment = 70
-    provided_data = 4
-    extra_data = 10
-elif package_type == ('purple'):
-    monthly_payment = 99.95
-else:
-    print('Invalid package type: please pick either green, blue, or purple')
+while passthru1 == "no":
+    if package_type == ('green'):
+        passthru1 = "yes"
+        monthly_payment = 49.99
+        provided_data = 2
+        extra_data = 15
+    elif package_type == ('blue'):
+        passthru1 = "yes"
+        monthly_payment = 70
+        provided_data = 4
+        extra_data = 10
+    elif package_type == ('purple'):
+        passthru1 = "yes"
+        monthly_payment = 99.95
+    else:
+        print('Invalid package type. please pick either green, blue, or purple')
 
 #calculates monthly cost for green and blue package type
 if package_type == ('green') or package_type == ('blue'):
-    used_data = input("enter amount of gigabytes used a month: ")
-    used_data = int(used_data)
-    data = used_data - provided_data
-    if (data > 0):
-        monthly_payment = (monthly_payment + (data * extra_data))
+    used_data = float(input("enter amount of gigabytes used this month: "))
+    data_difference = used_data - provided_data
+    if (data_difference > 0):
+        monthly_payment = (monthly_payment + (data_difference * extra_data))
 
 # checks if coupon is appliable and takes it into account if so applies it
+
 if package_type == ('green') and monthly_payment >= 75:
-    coupon = input('Do you have a coupon? yes or no: ')
-    coupon = coupon.lower()
-    if coupon == 'yes':
-        monthly_payment = monthly_payment - 20
+    while passthru2 == "no":
+        coupon_boolean = str.lower(input('Do you have a coupon? yes or no: '))
+        if coupon_boolean == 'yes':
+            passthru2 = "yes"
+            monthly_payment = monthly_payment - 20
+        elif coupon_boolean == 'no':
+            passthru2 = "yes"
+        else:
+            print("This is an invalid input. Please put in 'yes' or 'no' only")
+
 print(f'Your monthly payment is {monthly_payment:.2f} dollars.')
 
